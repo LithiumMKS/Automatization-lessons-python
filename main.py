408b32077bdc406952f3edcf39778403300675b0
Lithium <sandakov.maks@gmail.com>
2021-05-13 21:35:03 +0500

    check

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+grid = [['.', '.', '.', '.', '.', '.', ],
+        ['.', 'O', 'O', '.', '.', '.', ],
+        ['O', 'O', 'O', 'O', '.', '.', ],
+        ['O', 'O', 'O', 'O', 'O', '.', ],
+        ['.', 'O', 'O', 'O', 'O', 'O', ],
+        ['O', 'O', 'O', 'O', 'O', '.', ],
+        ['O', 'O', 'O', 'O', '.', '.', ],
+        ['.', 'O', 'O', '.', '.', '.', ],
+        ['.', '.', '.', '.', '.', '.', ]]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+'''
+for x in grid:
+    while grid.index(x) <= int(len(grid) -1):
+        xnum = 0
+            while y <= int(len(x) - 1):
+                print(x[y], end='')
+                y += 1
+        xnum += 1
+'''
+for y in range(len(grid[0])-1, -1, -1):
+    for x in range(len(grid)):
+        print(grid[x][y], end=' ')
+    print()
